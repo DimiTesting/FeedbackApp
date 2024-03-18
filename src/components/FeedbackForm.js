9a594ea2e3254019bb5efd22ee1e40a8f2560dbb
amankeldi.kurban <dimitesting31@gmail.com>
2024-03-18 16:59:23 +0200

    handleTextChange updated

--- src/components/FeedbackForm.js
+++ src/components/FeedbackForm.js
@@ -4,8 +4,20 @@ import Button from "./Button"
 
 function FeebackForm() {
     const [text, setText] = useState('')
+    const [isDisabled, setIsDisabled] = useState(true)
+    const [message, setMessage] = useState("")
 
     function handleTextChange(e) {
+        if (text === "") {
+            setIsDisabled(true)
+            setMessage(null)
+        } else if (text !=="" && text.trim().length<=10) {
+            setMessage("Text must be at least 10 characters")
+            setIsDisabled(true)
+        } else {
+            setMessage(null)
+            setIsDisabled(false)
+        }
         setText(e.target.value)
     }
 
@@ -15,9 +27,10 @@ function FeebackForm() {
                 <h2>How would you rate our services?</h2>
                 <div className="input-group">
                     <input type="text" placeholder="Write a review" value={text} onChange={handleTextChange}/>
-                    <Button type="submit"> Send </Button>
+                    <Button type="submit" isDisabled={isDisabled}> Send </Button>
                 </div>
             </form>
+            {message && <div className="message"> {message} </div>}
         </Card>
     )
 }
