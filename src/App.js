3374ccc00aeb3c868fa637526c132d1927d9593c
amankeldi.kurban <dimitesting31@gmail.com>
2024-03-20 14:23:45 +0200

    AboutIcanLink component with Link to Main page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutIconLink from "./components/AboutIconLink"
 import FeedbackData from "./FeedbackData"
 import AboutPage from './pages/AboutPage'
 import { useState } from "react"
@@ -38,6 +39,7 @@ const App = function App() {
                     <Route path="/about" element={<AboutPage/>}/>
                 </Routes>
                 </div>
+                <AboutIconLink/>
         </Router>
     )
 }
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,5 @@
 import Card from "../components/shared/Card"
+import {Link} from "react-router-dom"
 
 function AboutPage() {
     return (
@@ -6,6 +7,9 @@ function AboutPage() {
             <div className="about">
                 <h1>This an application which gathers a feedback</h1>
                 <p>Created by the React</p>
+                <p>
+                    <Link to="/"> Go back to the Feeback page</Link>
+                </p>
             </div>
         </Card>
     )
